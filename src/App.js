c57129eacce40f413f6a381a195f3095c14482d0
Eduardo010470 <EduardoFigueiredo63@gmail.com>
2026-06-06 20:29:20 -0300

    fix: remove unused useRef import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import { createClient } from "@supabase/supabase-js"
 import ReactMarkdown from "react-markdown"
 
